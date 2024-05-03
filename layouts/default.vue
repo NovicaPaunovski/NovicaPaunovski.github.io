3d0f42025be2de6573fb158b92d9d567dd594f90
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-05-04 00:59:16 +0200

    Replaced vue app with a nuxt.js app

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <header class="shadow-sm bg-white">
+           <nav class="container mx-auto p-4 flex justify-between items-center">
+              <NuxtLink to="/" class="font-bold flex gap-2 items-center mr-5">
+                <img class="w-48 min-w-24" src="../assets/content/logo.svg"/>
+              </NuxtLink>
+              <ul class="flex gap-4 items-center">
+                  <li><NuxtLink to="/"><p class="text-lg">Home</p></NuxtLink></li>
+              </ul>
+          </nav>
+      </header>
+      <div class="container mx-auto p-4">
+        <slot />
+      </div>
+      
+      <footer>
+
+      </footer>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+</style>
