f97f0295e5e8241f7bdded6bd3af46d7fa722e34
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-05-04 21:03:08 +0200

    Added temporary content to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="w-full h-full flex justify-center items-center">
-        <h1>Hello World</h1>
+    <div class="content w-full h-full flex justify-center items-center">
     </div>
 </template>
+
+<style scoped>
+    .content {
+        background-image: url("https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-02-16-at-5.26.01-PM.png");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+</style>
