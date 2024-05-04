af969d4e6c00d3fecdfdf44d12472337fbb98bed
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-05-04 22:23:59 +0200

    Removed background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="content w-full h-full flex justify-center items-center">
+    <div class="w-full h-full flex justify-center items-center">
+        <h1>Hello World</h1>
     </div>
 </template>
 
 <style scoped>
-    .content {
-        background-image: url("https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-02-16-at-5.26.01-PM.png");
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
 </style>
